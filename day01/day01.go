586a00e94c8b4cee17aa0e4d6941a87f904f2e1d
Tay0108 <kjkosecki@gmail.com>
2021-12-18 11:44:35 +0100

    feat: day01 part2 & fix part1

--- day01/day01.go
+++ day01/day01.go
@@ -8,33 +8,70 @@ import (
 	"strconv"
 )
 
-func main() {
-	fmt.Println("Day 01:")
-	file, error := os.Open("./input.txt") 
-	// fun fact - this ./ is where you are, not program
+// fun fact - this ./ is where you are, not program
+func loadFileToSlice(path string) []int {
+	file, error := os.Open(path)
 	if error != nil {
 		log.Fatal(error)
 	}
-
-	defer file.Close()
-
 	scanner := bufio.NewScanner(file)
 
-	counter := 0
-	prevValue := 124
+	var inputData []int
 
 	for scanner.Scan() {
-		currentValue, _ := strconv.Atoi(scanner.Text())
-		if(currentValue > prevValue) {
-			fmt.Println(currentValue, " is higher than ", prevValue)
+		num, _ := strconv.Atoi(scanner.Text())
+		inputData = append(inputData, num)
+	}
+
+	file.Close()
+
+	return inputData
+}
+
+func main() {
+	// part1:
+	input := loadFileToSlice("./input.txt")
+
+	counter := 0
+
+	for i := range input {
+		if i == 0 {
+			continue
+		}
+		if input[i] > input[i-1] {
 			counter++
 		}
-		prevValue = currentValue
 	}
 
-	if error := scanner.Err(); error != nil {
-		log.Fatal(error)
+	fmt.Println("counter", counter)
+
+	// part2:
+	sumCounter := 0
+	prevSum := 0
+
+	for i := range input {
+		if i == 0 {
+			prevSum = input[i] + input[i+1] + input[i+2]
+			continue
+		}
+
+		currentSum := input[i]
+
+		if i+1 < len(input) {
+			currentSum += input[i+1]
+
+			if i+2 < len(input) {
+				currentSum += input[i+2]
+			}
+		}
+
+		if currentSum > prevSum {
+			sumCounter++
+		}
+
+		prevSum = currentSum
 	}
 
-	fmt.Println(counter)
+	fmt.Println("Sum counter", sumCounter)
+
 }
